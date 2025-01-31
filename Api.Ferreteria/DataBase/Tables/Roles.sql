11819feddbf23b9794a65c8dac71b59028515477
Valeria <83665852+valerodriguezbz@users.noreply.github.com>
2025-01-31 17:00:29 -0600

    Role and User tables updated

--- Api.Ferreteria/DataBase/Tables/Roles.sql
+++ Api.Ferreteria/DataBase/Tables/Roles.sql
@@ -1,5 +1,9 @@
 ﻿CREATE TABLE [dbo].[Roles]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Type] VARCHAR(50) NOT NULL
+    [Type] VARCHAR(50) NOT NULL, 
+    [Created_at] DATETIME NULL, 
+    [Updated_at] DATETIME NULL, 
+    [User_created] INT NULL, 
+    [User_updated] INT NULL
 )
--- Api.Ferreteria/DataBase/Tables/Users.sql
+++ Api.Ferreteria/DataBase/Tables/Users.sql
@@ -3,5 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [IdPerson] INT NOT NULL, 
     [PasswordHash] VARCHAR(255) NOT NULL, 
+    [Created_at] DATETIME NULL, 
+    [Updated_at] DATETIME NULL, 
+    [user_created] INT NULL, 
+    [user_modified] INT NULL, 
     CONSTRAINT [FK_Users_ToPeople] FOREIGN KEY (IdPerson) REFERENCES People(Id)
 )
